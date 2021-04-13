62bed5a58980805a1f905d94dd3bc4ef3e5bb56b
Josef Leyva <jalm_x@hotmail.com>
2021-04-13 07:28:27 -0500

    index

---
+++ book/cap2/code_class/decision.py
@@ -0,0 +1,18 @@
+# Estructura
+print("Da tu edad")
+edad = int(input()  )
+
+if edad >= 18:
+    print("Eres mayor de edad")
+    if edad > 50:
+        print("ya estas cerca de la tercera edad")
+
+if edad < 18:
+    print("Eres menor de edad")
+
+print("Gracias por usar mi programa")
+'''
+Generar un programa que pida al usuario cuantas mascotas tiene, 
+si tiene igual o mas de 3, el programa le dice "eres amantes de las mascotas"
+si tiene menos de 3, le dira "necesitas un michi"
+'''
---
+++ book/cap2/code_class/michi.py
@@ -0,0 +1,8 @@
+print("Cuantas mascostas tienes?")
+mascotas = int(input())  
+
+if mascotas >= 3:
+    print("Eres amante de los animales")
+
+if mascotas < 3:
+    print("te falta un michi")
