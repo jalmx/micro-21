0618ee25252b4692b59da0c51879bbc681020d19
Josef Leyva <jalm_x@hotmail.com>
2021-04-30 14:15:51 -0500

    code_class del

--- book/cap2/code_class/michi.py
+++
@@ -1,8 +0,0 @@
-print("Cuantas mascostas tienes?")
-mascotas = int(input())  
-
-if mascotas >= 3:
-    print("Eres amante de los animales")
-
-if mascotas < 3:
-    print("te falta un michi")
--- book/cap2/code_class/suma.py
+++
@@ -1,4 +0,0 @@
-archivo = open('ruta del archivo')
-
-s = sum([2,3,6,9,7,87,5,6])
-print(s)
