da05c0f80b89b7bdcc6bfe06d29d4025190c6c58
Josef Leyva <jalm_x@hotmail.com>
2021-05-26 13:20:48 -0500

    servo 1

---
+++ book/cap3/practices/servo1.py
@@ -0,0 +1,33 @@
+from pyfirmata import Arduino, util
+from time import sleep
+
+PORT = '/tmp/ttyS1'
+board = Arduino(PORT)
+
+# Configuramos el pin que sera utilizado como PWM
+servo = board.get_pin('d:3:s') # el tercer parametro define que tipo sera la salida
+sleep(1)
+
+while True:
+    angulo = 0
+    servo.write(angulo)
+    sleep(2)
+    angulo = 30
+    servo.write(angulo)
+    sleep(2)
+    angulo = 60
+    servo.write(angulo)
+    sleep(2)
+    angulo = 90
+    servo.write(angulo)
+    sleep(2)
+    angulo = 120
+    servo.write(angulo)
+    sleep(2)
+    angulo = 160
+    servo.write(angulo)
+    sleep(2)
+    angulo = 180
+    servo.write(angulo)
+    sleep(2)
+
